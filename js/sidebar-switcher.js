f94856e8899795d4b3aa1633f36bd6612b4b04e4
rvck2007 <sekaherve@gmail.com>
2021-05-02 22:17:00 +0200

    Animer l'affichage et la disparition de la Sidebar
    Appliquer une Transition

--- js/sidebar-switcher.js
+++ js/sidebar-switcher.js
@@ -1,3 +1,34 @@
+
+( function($) {
+
+    // clic sur le 1er bouton de séleciton de mise en page :
+    $('a.sidebar-left-toggle').click(function(){
+        if(!$('.sidebar').hasClass('sidebar-left')){
+            $('.sidebar').addClass('sidebar-left');
+        }
+    })
+
+    // clic sur le 2ème bouton de séleciton de mise en page :
+    $('a.sidebar-right-toggle').click(function(){
+        if($('.sidebar').hasClass('sidebar-left')){
+            $('.sidebar').removeClass('sidebar-left');
+        }
+    })
+
+    // clic sur le 3ème bouton de sélection de mise en page :
+    $('a.no-sidebar-toggle').click(function(){
+        $('.content').toggleClass('no-sidebar');
+    })
+
+    // clic sur le 4ème bouton de sélection de mise en page :
+    $('a.hide-sidebar-toggle').click(function(){
+        $('.sidebar').toggleClass('hide');
+    })
+
+})( jQuery );
+
+
+/*
 ( function( $ ) {
     
     $('a.sidebar-left-toggle').click(function() {
@@ -29,7 +60,7 @@
     });
     
 })( jQuery );
-
+*/
 
 
 
